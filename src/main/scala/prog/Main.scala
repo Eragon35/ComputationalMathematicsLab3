f701e751d50b97ae2b182a19bef11720ddd327c9
Eragon35 <antipin35@yahoo.com>
2021-03-24 01:09:54 +0300

    Make Simpson rule

--- src/main/scala/prog/Main.scala
+++ src/main/scala/prog/Main.scala
@@ -14,15 +14,18 @@ object Main {
         | Метод прямоугольников (при n=4)
         | Заданная функция 1: −3x^3 − 5x^2 + 4x - 2
         | Функция 2: cos(x)
-        | Функция 3: x^3 − 0.78x^2 − 0.826x + 0,145""".stripMargin)
+        | Функция 3: x^3 − 0.78x^2 − 0.826x + 0.145
+        | Функция 4: −1.38x^3 − 5.42x^2 + 2.57x + 10.95""".stripMargin)
 
     while (true) {
+      println("\nВыберите функцию, набрав её номер")
       functionHandler(StdIn.readLine()) match {
         case Some(value) =>
           println("Введите пределы интегрирования через пробел")
           val limits = limitHandler(StdIn.readLine())
           Trapeziodal.solve(value, limits._1, limits._2, limits._3)
           Rectangle.solve(value, limits._1, limits._2, limits._3)
+          Simpson.solve(value, limits._1, limits._2)
         case None => Console.err.println("Some error")
       }
     }
--- src/main/scala/prog/MathMethods/Simpson.scala
+++ src/main/scala/prog/MathMethods/Simpson.scala
@@ -1,8 +1,18 @@
 package prog.MathMethods
 
 object Simpson {
-  def solve(func: Double => Double, left: Double, right: Double, step: Double): Unit = {
+  def solve(func: Double => Double, left: Double, right: Double): Unit = {
     
+    val answer = (right - left) / 6 * (func(left) + func(right) + 4 * func((right - left) / 2)) // use Simpson's 1/3 rule
+    println(s"Метод Симпсона = $answer")
   }
-
+  
+//  if you want Simpson's rule with n = 4
+//  val h = (right - left) / 4
+//  val leftAnswer = func(left)
+//  val rightAnswer = func(right)
+//  val middleAnswer = func((right - left) / 2)
+//  val leftQuatterAnswer = func(left + h)
+//  val rightQuatterAnswer = func(right - h)
+//  val answer = h / 3 * (leftAnswer + 4 * (leftQuatterAnswer + rightQuatterAnswer) + 2 * middleAnswer + rightAnswer)
 }
