44f19484842f4728f4e4ed684d37f2bfe5b57481
Eragon35 <antipin35@yahoo.com>
2021-03-26 23:33:30 +0300

    Some bullshit

--- src/main/scala/prog/MathMethods/Simpson.scala
+++ src/main/scala/prog/MathMethods/Simpson.scala
@@ -14,9 +14,19 @@ object Simpson {
         else findIntegral(x+step, answer + 4 * func(x))
       }
     }
+
+    @tailrec
+    def findIntegralN(n: Int, answer: Double): Double = {
+      if (left + step * n >= right - step) answer
+      else {
+        if (n % 2 == 0) findIntegralN(n+1, answer + 2 * func(left + step * n))
+        else findIntegralN(n+1, answer + 4 * func(left + step * n))
+      }
+    }
     val answer = ((right - left) / n) / 3 * findIntegral(left + step, func(left) + func(right))
+    val answerN = ((right - left) / n) / 3 * findIntegralN(1, func(left) + func(right))
     val simpleAnswer = (right - left) / 6 * (func(left) + func(right) + 4 * func((right + left) / 2)) // use Simpson's 1/3 rule
 
-    println(s"Метод Симпсона:\n\tПравило 1/3 = $simpleAnswer\n\tПри n = 4 ответ = $answer")
+    println(s"Метод Симпсона:\n\tПравило 1/3 = $simpleAnswer\n\tПри n = 4 ответ = $answerN метод без N = $answer")
   }
 }
